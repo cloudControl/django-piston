ee1b4621a2ce73e51c3e5bc6c13fddaf6cf98e9a
Jesper Noehr <jesper@noehr.org>
2009-05-10 18:44:42 +0200

    adding test for incoming json/yaml in tests/

--- tests/test_project/apps/testapp/models.py
+++ tests/test_project/apps/testapp/models.py
@@ -1,10 +1,14 @@
-
-copyrev: 86219b569b2b1e799d0c69a907021e856115d51a
-copy: test_project/apps/testapp/models.py
-
 from django.db import models
 
 class TestModel(models.Model):
     test1 = models.CharField(max_length=1, blank=True, null=True)
     test2 = models.CharField(max_length=1, blank=True, null=True)
-    pass
+    
+class ExpressiveTestModel(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    
+class Comment(models.Model):
+    parent = models.ForeignKey(ExpressiveTestModel, related_name='comments')
+    content = models.TextField()
+    
--- tests/test_project/apps/testapp/urls.py
+++ tests/test_project/apps/testapp/urls.py
@@ -1,22 +1,21 @@
-
-copyrev: 1d4cf18af95f536273d0e53a608a36488a64cb78
-copy: test_project/apps/testapp/urls.py
-
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
 
-from test_project.apps.testapp.handlers import EntryHandler
+from test_project.apps.testapp.handlers import EntryHandler, ExpressiveHandler
 
 auth = HttpBasicAuthentication(realm='TestApplication')
 
 entries = Resource(handler=EntryHandler, authentication=auth)
+expressive = Resource(handler=ExpressiveHandler, authentication=auth)
 
 urlpatterns = patterns('',
     url(r'^entries/$', entries),
     url(r'^entries/(?P<pk>.+)/$', entries),
     url(r'^entries\.(?P<emitter_format>.+)', entries),
     url(r'^entry-(?P<pk>.+)\.(?P<emitter_format>.+)', entries),
+    
+    url(r'^expressive\.(?P<emitter_format>.+)$', expressive),
 )
 
 
