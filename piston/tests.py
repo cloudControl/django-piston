391a04b87edd1840d25adbbb38268b2e873aaf40
Denis Cornehl <dc@cloudcontrol.de>
2012-06-13 12:26:26 +0200

    Revert "Update to current Version"
    This reverts commit fba8017fdebc86faf5642379a403baecfcca29ae.

--- piston/tests.py
+++ piston/tests.py
@@ -1,5 +1,4 @@
 # Django imports
-import django
 from django.core import mail
 from django.contrib.auth.models import User
 from django.conf import settings
@@ -15,73 +14,73 @@ from utils import rc
 from resource import Resource
 
 #class ConsumerTest(TestCase):
-    #fixtures = ['models.json']
-
-    #def setUp(self):
-        #self.consumer = Consumer()
-        #self.consumer.name = "Piston Test Consumer"
-        #self.consumer.description = "A test consumer for Piston."
-        #self.consumer.user = User.objects.get(pk=3)
-        #self.consumer.generate_random_codes()
-
-    #def _pre_test_email(self):
-        #template = "piston/mails/consumer_%s.txt" % self.consumer.status
-        #try:
-            #loader.render_to_string(template, {
-                #'consumer': self.consumer,
-                #'user': self.consumer.user
-            #})
-            #return True
-        #except TemplateDoesNotExist:
-            #"""
-            #They haven't set up the templates, which means they might not want
-            #these emails sent.
-            #"""
-            #return False
-
-    #def test_create_pending(self):
-        #""" Ensure creating a pending Consumer sends proper emails """
-        ## Verify if the emails can be sent
-        #if not self._pre_test_email():
-            #return
-
-        ## If it's pending we should have two messages in the outbox; one
-        ## to the consumer and one to the site admins.
-        #if len(settings.ADMINS):
-            #self.assertEquals(len(mail.outbox), 2)
-        #else:
-            #self.assertEquals(len(mail.outbox), 1)
-
-        #expected = "Your API Consumer for example.com is awaiting approval."
-        #self.assertEquals(mail.outbox[0].subject, expected)
-
-    #def test_delete_consumer(self):
-        #""" Ensure deleting a Consumer sends a cancel email """
-
-        ## Clear out the outbox before we test for the cancel email.
-        #mail.outbox = []
-
-        ## Delete the consumer, which should fire off the cancel email.
-        #self.consumer.delete()
-
-        ## Verify if the emails can be sent
-        #if not self._pre_test_email():
-            #return
-
-        #self.assertEquals(len(mail.outbox), 1)
-        #expected = "Your API Consumer for example.com has been canceled."
-        #self.assertEquals(mail.outbox[0].subject, expected)
+#    fixtures = ['models.json']
+#
+#    def setUp(self):
+#        self.consumer = Consumer()
+#        self.consumer.name = "Piston Test Consumer"
+#        self.consumer.description = "A test consumer for Piston."
+#        self.consumer.user = User.objects.get(pk=3)
+#        self.consumer.generate_random_codes()
+#
+#    def _pre_test_email(self):
+#        template = "piston/mails/consumer_%s.txt" % self.consumer.status
+#        try:
+#            loader.render_to_string(template, {
+#                'consumer': self.consumer,
+#                'user': self.consumer.user
+#            })
+#            return True
+#        except TemplateDoesNotExist:
+#            """
+#            They haven't set up the templates, which means they might not want
+#            these emails sent.
+#            """
+#            return False
+#
+#    def test_create_pending(self):
+#        """ Ensure creating a pending Consumer sends proper emails """
+#        # Verify if the emails can be sent
+#        if not self._pre_test_email():
+#            return
+#
+#        # If it's pending we should have two messages in the outbox; one
+#        # to the consumer and one to the site admins.
+#        if len(settings.ADMINS):
+#            self.assertEquals(len(mail.outbox), 2)
+#        else:
+#            self.assertEquals(len(mail.outbox), 1)
+#
+#        expected = "Your API Consumer for example.com is awaiting approval."
+#        self.assertEquals(mail.outbox[0].subject, expected)
+#
+#    def test_delete_consumer(self):
+#        """ Ensure deleting a Consumer sends a cancel email """
+#
+#        # Clear out the outbox before we test for the cancel email.
+#        mail.outbox = []
+#
+#        # Delete the consumer, which should fire off the cancel email.
+#        self.consumer.delete()
+#
+#        # Verify if the emails can be sent
+#        if not self._pre_test_email():
+#            return
+#
+#        self.assertEquals(len(mail.outbox), 1)
+#        expected = "Your API Consumer for example.com has been canceled."
+#        self.assertEquals(mail.outbox[0].subject, expected)
 
 
 class CustomResponseWithStatusCodeTest(TestCase):
      """
-     Test returning content to be formatted and a custom response code from a
-     handler method. In this case we're returning 201 (created) and a dictionary
-     of data. This data will be formatted as json.
+     Test returning content to be formatted and a custom response code from a 
+     handler method. In this case we're returning 201 (created) and a dictionary 
+     of data. This data will be formatted as json. 
      """
 
      def test_reponse_with_data_and_status_code(self):
-         response_data = dict(complex_response=dict(something='good',
+         response_data = dict(complex_response=dict(something='good', 
              something_else='great'))
 
          class MyHandler(BaseHandler):
@@ -101,10 +100,9 @@ class CustomResponseWithStatusCodeTest(TestCase):
          response = resource(request, emitter_format='json')
 
          self.assertEquals(201, response.status_code)
-         is_string = (not response._base_content_is_iter) if django.VERSION >= (1,4) else response._is_string
-         self.assert_(is_string, "Expected response content to be a string")
+         self.assertTrue(response._is_string, "Expected response content to be a string")
 
-         # compare the original data dict with the json response
+         # compare the original data dict with the json response 
          # converted to a dict
          self.assertEquals(response_data, simplejson.loads(response.content))
 
@@ -112,7 +110,7 @@ class CustomResponseWithStatusCodeTest(TestCase):
 class ErrorHandlerTest(TestCase):
     def test_customized_error_handler(self):
         """
-        Throw a custom error from a handler method and catch (and format) it
+        Throw a custom error from a handler method and catch (and format) it 
         in an overridden error_handler method on the associated Resource object
         """
         class GoAwayError(Exception):
@@ -122,7 +120,7 @@ class ErrorHandlerTest(TestCase):
 
         class MyHandler(BaseHandler):
             """
-            Handler which raises a custom exception
+            Handler which raises a custom exception 
             """
             allowed_methods = ('GET',)
 
@@ -131,16 +129,16 @@ class ErrorHandlerTest(TestCase):
 
         class MyResource(Resource):
             def error_handler(self, error, request, meth, em_format):
-                # if the exception is our exeption then generate a
-                # custom response with embedded content that will be
-                # formatted as json
+                # if the exception is our exeption then generate a 
+                # custom response with embedded content that will be 
+                # formatted as json 
                 if isinstance(error, GoAwayError):
                     response = rc.FORBIDDEN
                     response.content = dict(error=dict(
-                        name=error.name,
-                        message="Get out of here and dont come back",
+                        name=error.name, 
+                        message="Get out of here and dont come back", 
                         reason=error.reason
-                    ))
+                    ))    
 
                     return response
 
@@ -154,7 +152,7 @@ class ErrorHandlerTest(TestCase):
 
         self.assertEquals(403, response.status_code)
 
-        # verify the content we got back can be converted back to json
+        # verify the content we got back can be converted back to json 
         # and examine the dictionary keys all exist as expected
         response_data = simplejson.loads(response.content)
         self.assertTrue('error' in response_data)
@@ -164,7 +162,7 @@ class ErrorHandlerTest(TestCase):
 
     def test_type_error(self):
         """
-        Verify that type errors thrown from a handler method result in a valid
+        Verify that type errors thrown from a handler method result in a valid 
         HttpResonse object being returned from the error_handler method
         """
         class MyHandler(BaseHandler):
@@ -175,7 +173,7 @@ class ErrorHandlerTest(TestCase):
         request.method = 'GET'
         response = Resource(MyHandler)(request)
 
-        self.assertTrue(isinstance(response, HttpResponse), "Expected a response, not: %s"
+        self.assertTrue(isinstance(response, HttpResponse), "Expected a response, not: %s" 
             % response)
 
 
@@ -196,5 +194,5 @@ class ErrorHandlerTest(TestCase):
         request.method = 'GET'
         response = resource(request)
 
-        self.assertTrue(isinstance(response, HttpResponse), "Expected a response, not: %s"
+        self.assertTrue(isinstance(response, HttpResponse), "Expected a response, not: %s" 
             % response)
